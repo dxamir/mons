19413cb33fd951afdb85b99fc83d451e03b5d713
sssilver <sssilver@371ced2d-314e-0410-9552-e300cb327ffc>
2008-05-27 08:03:30 +0000

    Very initial commit

---
+++ trunk/mons/Game.inc.php
@@ -0,0 +1,22 @@
+<?
+
+	require_once('Player.inc.php');
+
+
+	class Game
+	{
+		var $currentPlayer;
+
+
+		function rollDice($player)
+		{
+			$die1 = rand(1, 6);
+			$die2 = rand(1, 6);
+		}
+		
+		
+	}
+
+
+
+?>
