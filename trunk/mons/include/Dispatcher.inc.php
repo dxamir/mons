1e55eb7d0fbcfd2a007658e416da8abd8b40072c
sssilver <sssilver@371ced2d-314e-0410-9552-e300cb327ffc>
2008-05-30 08:59:20 +0000

    Added phpDoc

--- trunk/mons/include/Dispatcher.inc.php
+++ trunk/mons/include/Dispatcher.inc.php
@@ -1,14 +1,26 @@
 <?php
-/*
- * Created on Jan 28, 2008
- */
- 
+
+	/**
+	 * Dispatcher class
+	 * 
+	 * @author sssilver@gmail.com
+	 * @todo Finish the dispatch() function
+	 */
+
+
  	require_once('Command.inc.php');
 
- 
+
+
 	class Dispatcher
 	{
 	
+		/**
+		 * Parses the request map, returning a command
+		 *
+		 * @param map $request
+		 * @return Command
+		 */
 		function parse($request)
 		{
 			$action = 'a';
@@ -19,7 +31,12 @@
 			return new Command($request[$action], $request[$uid], $request[$gid], $request[$param]);
 		}
 
-		
+		/**
+		 * Validates the given command, making sure its game exists and is active and the command is valid
+		 *
+		 * @param Command $command
+		 * @return bool
+		 */
 		function validate($command)
 		{
 			$games = $_SESSION['games'];		// active games instances
@@ -30,17 +47,20 @@
 			if ($games && !array_key_exists($command->gid, $games))
 				return false;
 				
-				
 			// check the command exists
 			if (!method_exists($game, 'cmd' . $command->action))
 				return false;
 				
 				
 			return true;
-			
 		}
 		
-		
+
+		/**
+		 * Main function for dispatching. Dispatches the given request to the matching command and returns the response.
+		 *
+		 * @param map $request
+		 */
 		function dispatch($request)
 		{
 			$command = $this->parse($request);
