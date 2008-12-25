b8251b8893670edea636fe0929c2f63cb736dddc
sssilver <sssilver@371ced2d-314e-0410-9552-e300cb327ffc>
2008-12-25 11:17:25 +0000

    Monopoly game class stuf and event mechanism implementation, sample action Tell

---
+++ mow/index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+
+<html>
+
+<head>
+	<title>mow: Monopoly Web</title>
+</head>
+
+<body>
+
+	<h1 style="float:left;">mow | </h1>
+	<h4 style="padding-top:12px; text-indent:5px;">Aid to mons development as a mons client simulator.</h4>
+
+	<label>Action</label> <input type="text" id="a" value="
+
+
+</body>
+
+</html>
